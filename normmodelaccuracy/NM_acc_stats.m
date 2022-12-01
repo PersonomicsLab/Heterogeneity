178449a235f1253a52669a21a8a7c8c2cf96c0a7
Kayla Hannon <74215929+kahannon@users.noreply.github.com>
2022-12-01 14:29:21 -0600

    Delete normmodelaccuracy directory

--- normmodelaccuracy/NM_acc_stats.m
+++
@@ -1,8 +0,0 @@
-SMSE = readmatrix('SMSE_estimate.txt');
-RMSE = readmatrix('RMSE_estimate.txt');
-Rho = readmatrix('Rho_estimate.txt');
-pRho = readmatrix('pRho_estimate.txt');
-MSLL = readmatrix('MSLL_estimate.txt');
-EXPV = readmatrix('EXPV_estimate.txt');
-
-NMacc = table(SMSE, RMSE, Rho, pRho, MSLL, EXPV);
